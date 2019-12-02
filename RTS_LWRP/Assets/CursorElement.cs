b84fe0c8d4316be50c69938ecb4d66341591ee9d
Pokoi <jesusferminvillar@gmail.com>
2019-12-02 19:42:02 +0100

    NavMesh generated

---
+++ RTS_LWRP/Assets/CursorElement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CursorElement : MonoBehaviour
+{
+
+    static  CursorElement   instance;
+    private NavMeshAgent    selfAgent;
+    private Transform       selfTransform;
+    public  Camera          cameraToRayCast;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        selfAgent       = GetComponent<NavMeshAgent>();
+        selfTransform   = transform;
+
+        StartCoroutine(SetPosition(1.0f));
+    }
+
+   
+
+    IEnumerator SetPosition(float delta)
+    {
+        while (true)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(cameraToRayCast.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                selfAgent.destination = hit.point;
+            }
+
+                yield return new WaitForSeconds(delta);
+        }
+
+        
+    }
+}
---
+++ RTS_LWRP/Assets/NavAgentFollowCursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentFollowCursor : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+   
+
+    IEnumerator FollowingCursor()
+    {
+        //while()
+        yield return new WaitForSeconds(1);
+    }
+
+
+}
